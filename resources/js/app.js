a8e3c5d91ac94e65f30817c09fd85f3b898dda0f
mxwebdev <maxwoerner@gmail.com>
2022-07-28 13:05:36 +0200

    Removed livewire-sortable CDN dependency

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
 import './bootstrap';
 
+import '@nextapps-be/livewire-sortablejs';
+
 import Alpine from 'alpinejs';
 import collapse from '@alpinejs/collapse';
 
--- resources/views/livewire/setlist-builder.blade.php
+++ resources/views/livewire/setlist-builder.blade.php
@@ -110,9 +110,3 @@
 
     </div>
 </div>
-
-@push('scripts')
-@once
-<script src="https://unpkg.com/@nextapps-be/livewire-sortablejs@0.2.0/dist/livewire-sortable.js"></script>
-@endonce
-@endpush
